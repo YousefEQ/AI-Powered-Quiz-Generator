8ca464507dcbae5b2b5f8df5c71d2ba05c7cc1de
Yousef EL-Qawasmi <56516358+YousefEQ@users.noreply.github.com>
2023-12-04 22:40:37 +0300

    quiz_generator.py

--- quiz_generator.py
+++ quiz_generator.py
@@ -3,15 +3,15 @@ from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage
 
 class QuizGenerator:
-    """A class to generate unique quiz questions using OpenAI's ChatGPT."""
+    #a class to generate unique quiz questions using openai chatgpt
 
     def __init__(self, openai_api_key, model_name="gpt-3.5-turbo"):
-        """Initialize the ChatOpenAI model with the given API key and model name."""
+        #initialize the chatopenai model with the given API key and model name
         self.chat_model = ChatOpenAI(model_name=model_name, openai_api_key=openai_api_key)
         self.generated_questions = set()
 
     def generate_question(self, topic):
-        """Generate a unique quiz question on the given topic."""
+        #generate a unique quiz question on the given topic
         retry_limit = 5
         for _ in range(retry_limit):
             prompt = f"Create a unique multiple choice question about {topic}. make sure you don't ask a question more than once. Include 4 answer choices and indicate the correct one."
